1b455626e7bf20df5f36cb6432b347bc4c661ced
Sabit khan <mesabitkhan@gmail.com>
2023-07-10 09:46:06 +0500

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, List, ListItem } from "@chakra-ui/layout";
+import { HStack, Heading, List, ListItem } from "@chakra-ui/layout";
 import useGenres, { Genre } from "../hook/useGenre";
 import { Button, Image, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
@@ -11,18 +11,24 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, loading, error } = useGenres();
   return (
     <div>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
       <List>
         {loading && <Spinner />}
         {data.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
+                objectFit="cover"
                 boxSize="32px"
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
               />
               <Button
-                fontWeight={genre.id === selectedGenre?.id?'bold':'normal'}
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
                 variant="link"
